d35b1a37f27a37d586ff0615c5cb333919ea10d2
k22036 <131113333+k22036@users.noreply.github.com>
2024-06-27 10:32:45 +0900

    fix: ログイン後に/homeに遷移するように変更

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
+    return redirect(route('home'));
+});
+
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/home', function () {
     return view('pages/home');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
