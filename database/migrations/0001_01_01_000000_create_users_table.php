da2adf441e18bce4ef9e5aeae0f31600b8b047e6
k22036 <131113333+k22036@users.noreply.github.com>
2024-07-01 15:59:02 +0900

    fix: user_idをランダムなuuidとなるように修正

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid('user_id')->default(Str::uuid());
+            $table->uuid('user_id')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
